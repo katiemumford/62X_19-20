75d5bbd3a2b862c817f78acc3f790ca9be8db18a
Katie Mumford <katieamumford@gmail.com>
2019-09-24 15:17:18 -0700

    test

--- Traybot/TraybotMain/include/common.h
+++ Traybot/TraybotMain/include/common.h
@@ -5,9 +5,19 @@
 #include <vector>
 using namespace vex;
 
+/**dean code
 int minPct = 5; //minimum controller value (%) for drive, accounts for stick drift
 bool intaking = false;  //whether the intake has been toggled
 int intakeWait = 0; //time since last toggle
+**/
+
+//katie code, adapted dean's intake toggle code for the arm
+int minPct = 5; //minimum controller value (%) for drive, accounts for stick drift
+bool armMoving = false;  //whether the arm has been toggled
+int armWait = 0; //time since last arm toggle
+
+bool tiltingForward = false; //whether the tilt has been toggled to go forward
+int tiltWait = 0; // time since last titl forward toggle
 
 int maxPotVal = 1288;
 int restPotVal = 2780;
@@ -69,7 +79,35 @@ void moveTray(int pct) {
 //////////DRIVER_FUNCTIONS//////////
 #pragma region
 
+/**
+Sept 23, 2019
+Katie wants to change the program 
+**/ 
+
 void intakeControl() {
+
+//if R1 is pressed, intake intakes at 100 until unpressed
+//if R2 pressed, intake outakes at -100 until unpressed
+//if R1 and (shiftKey) L2 pressed, intake intakes at 50 until unpressed
+//if R2 and (shiftKey) L2 pressed, intake outakes at -50 until unpressed
+if (Controller.ButtonR1.pressing()){
+  spinIntake(100);
+}
+if(Controller.ButtonR2.pressing()){
+  spinIntake(-100);
+}
+if (Controller.ButtonR1.pressing() && Controller.ButtonL2.pressing()){
+  spinIntake(50);
+}
+if (Controller.ButtonR2.pressing() && Controller.ButtonL2.pressing()){
+  spinIntake(-50);
+}
+else{
+  spinIntake(0);
+}
+}
+  
+  /** dean code
   //if R1 is pressed 200ms since last press, toggle intake on/off
   if (Controller.ButtonR1.pressing() && vex::timer::system() > intakeWait + 200) {
     intaking = !intaking;
@@ -83,19 +121,62 @@ void intakeControl() {
   } else {
     spinIntake(0);
   }
-}
+  **/
+
+
+/**
+default you press R1 once and tray goes to stacked position (tuned)
+press the same button again and returns to intake position
+
+hold shift key and press R1 and R2(shift key) to get intake arm up to tower height
+hold shift key and press R1 again to get intake back down 
+**/
 
 void armControl() {
-  if (Controller.ButtonL1.pressing()) { //hold L1 to move intake arm up
-    moveArm(100);
-  } else if (Controller.ButtonL2.pressing()) {  //hold L2 to move intake arm down
-    moveArm(-70);
+
+ //if L2 and L1 are pressed 200ms since last press, toggle arm on/off
+  if (Controller.ButtonL1.pressing() && Controller.ButtonL2.pressing() && vex::timer::system() > armWait + 200) {
+    armMoving = true;
+    armWait = vex::timer::system();
+  } 
+  if (Controller.ButtonL1.pressing() && Controller.ButtonL2 && armMoving ) { //if holding both buttons for the first time
+    moveArm(100);  //move arm up to tower height and power 
+    armMoving = false;
+  } else if (Controller.ButtonL1.pressing() && Controller.ButtonL2 && !armMoving ) {
+    moveArm(-100); 
+    //figure out exact amount to go back down
+    //moveArm needs macro to tilt tray a little bit
   } else {
     moveArm(0);
   }
+
+  /** dean code
+  if (Controller.ButtonL1.pressing()) { //hold L1 to move intake arm up
+    moveArm(100);
+  } else if (Controller.ButtonL2.pressing()) {  //hold L2 to move intake arm down
+    moveArm(-70); 
+  } else {
+    moveArm(0);
+  } **/
 }
 
 void trayControl() {
+
+//if L1 is pressed 200ms since last press, toggle tilt on/off for forwards then backwards
+  if (Controller.ButtonL1.pressing() && vex::timer::system() > tiltWait + 200) {
+    tiltingForward = true;    //don't totally know what this does but dean had it?
+    tiltWait = vex::timer::system();
+  } 
+  if (Controller.ButtonR1.pressing() && titltingForward) { //if toggle already pressed and titling forward,
+    moveTray(100); //FIGURE OUT PROPER TILt TO GO FORWARD
+    tiltingForward = false; 
+  } else if (Controller.ButtonL1.pressing() && !tiltingForward) { //hold B to intake (optional)
+    moveTray(-100); //FIGURE OUT PROPER TILT TO GO BACK
+  } else {
+    moveTray(0);
+  }
+
+  //manual control buttons 
   if (Controller.ButtonX.pressing()) {  //hold X to move tray up
     moveTray(80);
   } else if (Controller.ButtonA.pressing()) {  //hold A to move tray down
